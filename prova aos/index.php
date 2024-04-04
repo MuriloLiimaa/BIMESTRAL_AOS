00499f8e4e9903b180a652cc2d5c63b31dee7e03
MuriloLiimaa <144131524+MuriloLiimaa@users.noreply.github.com>
2024-04-04 08:51:08 -0300

    Add files via upload

---
+++ prova aos/index.php
@@ -0,0 +1,37 @@
+<?php
+    //1
+   function helloworld(){
+    echo 'Hello World';
+   }
+
+    //2
+    function name(){
+        echo 'digite seu nome' = $nomerecebido;
+        return $nomerecebido;
+    } 
+    echo 'Hello' (name()); 
+
+    //3
+    $alunos = ['Marques', 'Murilo', 'Samuel', 'Bruno', 'Ricardo', 'Jorge'];
+    echo $alunos[4];
+    
+   //4
+   $numeros = [1, -2, -3, -4, 5];
+   
+   //5
+    echo $numeros [1,2,3];
+    echo $numeros [0,4];
+
+   //6
+
+
+   //7
+    $aluno = (
+        ["nome" => "Pedrinho"]
+        ["idade" => 17]
+        ["curso" => "Informatica"]
+    );
+
+    echo $aluno["nome", "idade", "curso"];
+
+?>
